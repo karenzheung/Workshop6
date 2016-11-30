2f5f28cfeaab64184dbaf8fc8d5541ca6759f642
Karen Zheung <karenzheung@gmail.com>
2016-11-29 21:59:36 -0500

    facebookGotServed

--- server/src/server.js
+++ server/src/server.js
@@ -1,68 +1,17 @@
+var bodyParser = require('body-parser');
+var serverModule = require('./database');
 // Imports the express Node module.
 var express = require('express');
-// Creates an Express server.
+var readDocument = serverModule.readDocument;
+//Creates an Express server.
 var app = express();
-// Import reverse String from util.js
-var util = require('./util');
-var reverseString = util.reverseString;
-var bodyParser = require('body-parser');
-var serverMod = require('./database');
-var readDocument = serverMod.readDocument;
-
-
-// Starts the server on port 3000!
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
 
 app.use(bodyParser.text());
-
-// You run the server from 'server', so '../client/build' is 'server/../client/build'
-// '..' means "go up one directory", so this translates into 'client/build!'
 app.use(express.static('../client/build'));
 
-/**
- * Get the user IF from a token. Returns -1 (an invalid ID)
- *if it fails.
- */
-function getUserIdFromToken(authorizationLine) {
-  try {
-    // Cut off "Bearer " from the header value.
-    var token = authorizationLine.slice(7);
-    // Convert the base64 string to a UTF-8 string.
-    var regualrString = new Buffer(token, 'base64').toString('utf8');
-    // Convert the UTF-8 string into a Javascript object.
-    var tokenObj = JSON.parse(regularString);
-    var id = tokenObj['id'];
-    // Check that id is a number.
-    if (typeof id === 'number') {
-      return id;
-    } else {
-      // Not a number. Return -1, an invalid ID.
-      return -1;
-    }
-  } catch (e) {
-    //Return an invalid ID.
-    return -1;
-  }
-}
-
-/**
- * Get the feed data for a particular user.
- */
-app.get('/user/:userid/feed', function(req, res) {
-  var userid = req.params.userid;
-  var fromUser = getUserIdFromToken(req.get('Authorization'));
-  // userid is a string. We need it to be a number.
-  // Parameters are always strings.
-  var useridNumber = parseInt(userid, 10);
-  if (fromUser === useridNumber) {
-    // Send response.
-    res.send(getFeedData(userid));
-  } else {
-    // 401: Unauthorized request.
-    res.status(401).end();
-  }
+//Starts the Server on port 3000!
+app.listen(3000,function() {
+  console.log('Example app listening on port 3000');
 });
 
 /**
@@ -82,16 +31,58 @@ function getFeedItemSync(feedItemId) {
   return feedItem;
 }
 
-
 /**
  * Emulates a REST call to get the feed data for a particular user.
  */
-function getFeedData(user, cb) {
+function getFeedData(user) {
   var userData = readDocument('users', user);
   var feedData = readDocument('feeds', userData.feed);
   // While map takes a callback, it is synchronous, not asynchronous.
   // It calls the callback immediately.
   feedData.contents = feedData.contents.map(getFeedItemSync);
-  //return FeedData with resolved references.
+  // Return FeedData with resolved references.
   return feedData;
+}
+
+/**
+ * Get the feed data for a particular user.
+*/
+app.get('/user/:userid/feed', function(req, res) {
+  var userid = req.params.userid;
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+  // userid is a string. We need it to be a number.
+  // Parameters are always strings.
+  var useridNumber = parseInt(userid, 10);
+  if (fromUser === useridNumber) {
+    // Send response.
+    res.send(getFeedData(userid)); } else {
+    // 401: Unauthorized request.
+    res.status(401).end();
+  }
+});
+
+/**
+ * Get the user ID from a token. Returns -1 (an invalid ID)
+ * if it fails.
+ */
+function getUserIdFromToken(authorizationLine) {
+  try {
+    // Cut off "Bearer " from the header value.
+    var token = authorizationLine.slice(7);
+    // Convert the base64 string to a UTF-8 string.
+    var regularString = new Buffer(token, 'base64').toString('utf8');
+    // Convert the UTF-8 string into a JavaScript object.
+    var tokenObj = JSON.parse(regularString);
+    var id = tokenObj['id'];
+    // Check that id is a number.
+    if (typeof id === 'number') {
+      return id;
+    } else {
+      // Not a number. Return -1, an invalid ID.
+      return -1;
+    }
+  } catch (e) {
+    // Return an invalid ID.
+    return -1;
+  }
 }
